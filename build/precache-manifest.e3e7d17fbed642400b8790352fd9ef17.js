4b53950371ab4e594150cc48670d339687b6cf54
inwein <39739420+inwein@users.noreply.github.com>
2020-05-25 16:36:32 +0300

    'final_build'

---
+++ build/precache-manifest.e3e7d17fbed642400b8790352fd9ef17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f043a3fafa1f9502404c29794dea40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec980b8702a43a149dc5",
+    "url": "./static/css/main.516eb43b.chunk.css"
+  },
+  {
+    "revision": "1ea7a1c2bf29756933df",
+    "url": "./static/js/2.b5807f90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b5807f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec980b8702a43a149dc5",
+    "url": "./static/js/main.fbb7ad95.chunk.js"
+  },
+  {
+    "revision": "92e78a015dae5315f7da",
+    "url": "./static/js/runtime-main.b7228439.js"
+  }
+]);
